373ead3dbc306e043c45bf0d170d13166dc5cc33
weiqing-nic <yoweiqing@gmail.com>
2019-04-08 19:27:56 +0800

    weiqing

--- raise_player.py
+++ raise_player.py
@@ -1,31 +1,40 @@
 from pypokerengine.players import BasePokerPlayer
-from time import sleep
+import random as rand
 import pprint
+from pypokerengine.utils.card_utils import gen_cards, estimate_hole_card_win_rate
+NB_SIMULATION = 1000
 
 class RaisedPlayer(BasePokerPlayer):
 
-  def declare_action(self, valid_actions, hole_card, round_state):
-    for i in valid_actions:
-        if i["action"] == "raise":
-            action = i["action"]
-            return action  # action returned here is sent to the poker engine
-    action = valid_actions[1]["action"]
-    return action # action returned here is sent to the poker engine
+    def declare_action(self, valid_actions, hole_card, round_state):
+        community_card = round_state['community_card']
+        win_rate = estimate_hole_card_win_rate(
+        nb_simulation=NB_SIMULATION,
+        nb_player=self.nb_player,
+        hole_card=gen_cards(hole_card),
+        community_card=gen_cards(community_card)
+        )
+        if win_rate >= 1.0 / self.nb_player:
 
-  def receive_game_start_message(self, game_info):
-    pass
+            action = valid_actions[1]  # fetch CALL action info
+        else:
+            action = valid_actions[0]  # fetch FOLD action info
+        return action['action']
 
-  def receive_round_start_message(self, round_count, hole_card, seats):
-    pass
+    def receive_game_start_message(self, game_info):
+        self.nb_player = game_info['player_num']
 
-  def receive_street_start_message(self, street, round_state):
-    pass
+    def receive_round_start_message(self, round_count, hole_card, seats):
+        pass
 
-  def receive_game_update_message(self, action, round_state):
-    pass
+    def receive_street_start_message(self, street, round_state):
+        pass
 
-  def receive_round_result_message(self, winners, hand_info, round_state):
-    pass
+    def receive_game_update_message(self, action, round_state):
+        pass
+
+    def receive_round_result_message(self, winners, hand_info, round_state):
+        pass
 
 def setup_ai():
   return RandomPlayer()
--- randomplayer.py
+++ randomplayer.py
@@ -12,17 +12,8 @@ import pandas as pd
 class RandomPlayer(BasePokerPlayer):
 
     def __init__(self):
-        self.table = {}
-        self.my_uuid = None
-        self.e = 0.1
-        self.experience_state = []
-        self.experience_reward = []
-        self.play = False
+        self.vvh = 0
 
-
-
-
-    def declare_action(self, valid_actions, hole_card, round_state):
         def keras_model():
 
             input_cards = Input(shape=(4,13,4), name="cards_input")
@@ -43,13 +34,28 @@ class RandomPlayer(BasePokerPlayer):
             out = Dense(3)(x)
 
             model = Model(inputs=[input_cards, input_actions,input_position], outputs=out)
+            if (self.vvh == 0):
+                model.load_weights('aaaaaa.h5', by_name=True)
             model.compile(optimizer='rmsprop', loss='mse')
-
-            #model.summary()
 
             return model
 
-        def get_card_X(card):
+        self.table = {}
+        self.my_uuid = None
+        self.e = 0.1
+        self.experience_state = []
+        self.experience_reward = []
+        self.play = False
+        self.model =keras_model()
+
+
+
+
+    def declare_action(self, valid_actions, hole_card, round_state):
+
+
+
+        def getcardx(card):
             suit = card[0]
             if(suit == 'S'):
                 return 0
@@ -60,7 +66,7 @@ class RandomPlayer(BasePokerPlayer):
             elif(suit=='C'):
                 return 3
 
-        def get_card_Y(card):
+        def getcardy(card):
             index = card[1]
             if(index=='A'):
                 return 12
@@ -75,13 +81,13 @@ class RandomPlayer(BasePokerPlayer):
             else:
                 return int(index)-2
 
-        def get_street_grid(cards):
+        def getstreetgrid(cards):
             grid = np.zeros((4,13))
             for card in cards:
-                grid[get_card_X(card),get_card_Y(card)] = 1
+                grid[getcardx(card),getcardy(card)] = 1
             return grid
 
-        def convert_street_to_image(eff_stack,round_state,street):
+        def converttoimagemeth(eff_stack,round_state,street):
             image = np.zeros((2,6))
             actions = round_state["action_histories"][street]
             index = 0
@@ -102,8 +108,8 @@ class RandomPlayer(BasePokerPlayer):
         #bb_cards
         sb_cards = [hole_card[0], hole_card[1]]
 
-        #bb_cards_img = get_street_grid(bb_cards)
-        sb_cards_img = get_street_grid(sb_cards)
+        #bb_cards_img = getstreetgrid(bb_cards)
+        sb_cards_img = getstreetgrid(sb_cards)
         flop_cards_img = np.zeros((4,13))
         turn_cards_img = np.zeros((4,13))
         river_cards_img = np.zeros((4,13))
@@ -111,12 +117,7 @@ class RandomPlayer(BasePokerPlayer):
         turn  = []
         river = []
 
-        #if round_state['small_blind_pos']['uuid']
-        #self.my_uuid = round_state['next_player']['uuid']
-        # print("my uuid")
-        # for attr in dir(round_state['next_player']['uuid']):
-        #     if hasattr( obj, attr ):
-        #         print( "obj.%s = %s" % (attr, getattr(obj, attr)))
+
         self.my_uuid =  round_state['seats'][round_state['next_player']]['uuid']
         self.my_cards =  hole_card
         self.community_card = round_state['community_card']
@@ -129,26 +130,26 @@ class RandomPlayer(BasePokerPlayer):
             self.oldAction = self.action_sb
 
         sb_position = 1
-        model = keras_model()
+
         flop_actions = np.zeros((2,6))
         turn_actions = np.zeros((2,6))
         river_actions = np.ones((2,6))
 
-        preflop_actions = convert_street_to_image(starting_stack,round_state,'preflop')
+        preflop_actions = converttoimagemeth(starting_stack,round_state,'preflop')
 
         if (round_state['street'] == 'flop'):
             flop = round_state['community_card']
-            flop_cards_img = get_street_grid(flop)
-            flop_actions = convert_street_to_image(starting_stack,round_state,'flop')
+            flop_cards_img = getstreetgrid(flop)
+            flop_actions = converttoimagemeth(starting_stack,round_state,'flop')
 
         if ( round_state['street'] == 'turn'):
             turn = round_state['community_card'][2]
-            turn_cards_img = get_street_grid([turn])
-            turn_actions = convert_street_to_image(starting_stack,round_state,'turn')
+            turn_cards_img = getstreetgrid([turn])
+            turn_actions = converttoimagemeth(starting_stack,round_state,'turn')
         if (round_state['street'] == 'river'):
             river = round_state['community_card'][3]
-            river_cards_img = get_street_grid([river])
-            river_actions = convert_street_to_image(starting_stack,round_state,'river')
+            river_cards_img = getstreetgrid([river])
+            river_actions = converttoimagemeth(starting_stack,round_state,'river')
 
         self.actions_feature = np.stack([preflop_actions,flop_actions,turn_actions,river_actions],axis=2).reshape((1,2,6,4))
         sb_cards_feature = np.stack([sb_cards_img,flop_cards_img,turn_cards_img,river_cards_img],
@@ -162,38 +163,38 @@ class RandomPlayer(BasePokerPlayer):
         # print(self.sb_features.shape)
         #if round_state['seats'][round_state['big_blind_pos']]['uuid'] == self.my_uuid:
         y = 0.9
-        max_replay_size = 50
+        max_replay_size = 40
         self.action_sb = 3
         #run model to choose action
-        self.allQ_sb = model.predict(self.sb_features)
+        self.allQ_sb = self.model.predict(self.sb_features)
         self.action_sb = np.argmax(self.allQ_sb)
         reward_sb = 0
         if(self.play == True):
             reward_sb += y*np.max(self.allQ_sb)
 
-            print("the reward is ")
-            print(reward_sb)
-            print(self.targetQ)
-            print("this is old state")
-            print(type(self.old_state))
-            # dfObj = pd.DataFrame(self.old_state[0])
-            # print(dfObj.shape)
+
             self.targetQ[0,self.action_sb] = reward_sb
+            self.vvh = self.vvh + 1
+            #new_name = 'my_model_weights'
             #model.fit(self.old_state,self.targetQ,verbose=0)
             self.experience_state.append(self.old_state)
             self.experience_reward.append(self.targetQ)
             if(len(self.experience_state) > max_replay_size):
                 del self.experience_state[0]
                 del self.experience_reward[0]
+                #new_name = new_name + str(self.vvh) + '.h5'
+                #self.model.save_weights(new_name)
+
 
         self.play = True
 
         e = 0.1
 
         for ve in range(len(self.experience_state)):
-            print("hello")
-            model.fit(self.experience_state[ve],self.experience_reward[ve],verbose = 0)
-            
+            #print("hello")
+            self.model.fit(self.experience_state[ve],self.experience_reward[ve],verbose = 0)
+            #print("finish")
+
 
         if(np.random.rand(1) < e):
             self.action_sb = np.random.randint(0,4)
@@ -217,31 +218,6 @@ class RandomPlayer(BasePokerPlayer):
             call_action_info = valid_actions[2]
             action = call_action_info["action"]
             return action
-
-
-
-        # print(round_state['small_blind_pos'])
-        #
-        # for g in round_state['action_histories']:
-        #     print(g)
-        # #if round_state['small_blind_pos'] == 0:
-
-
-
-
-
-        r = rand.random()
-        if r <= 0.5:
-            call_action_info = valid_actions[1]
-        elif r<= 0.9 and len(valid_actions ) == 3:
-            call_action_info = valid_actions[2]
-        else:
-            call_action_info = valid_actions[0]
-        action = call_action_info["action"]
-        return action  # action returned here is sent to the poker engine
-
-
-
 
 
 
