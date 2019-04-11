d2b13b4a77fc4b33e229a89889da936203d2c106
jasmoon <fourdusk4@gmail.com>
2019-04-11 20:34:16 +0800

    Refactor

--- raise_player.py
+++ raise_player.py
@@ -9,6 +9,11 @@ from pypokerengine.utils.game_state_utils import restore_game_state
 
 
 class Group18Player(BasePokerPlayer):
+    suits = {'S': 0, 'H': 1, 'D': 2, 'C': 3}
+    ranks = {'A': 12, 'K': 11, 'Q': 10, 'J': 9, 'T': 8, '9': 7, '8': 6, '7': 5, '6': 4, '5': 3, '4': 2, '3': 1, '2': 0}
+    y = 0.9
+    e = 0.1
+    max_replay_size = 40
 
     def __init__(self):
 
@@ -54,29 +59,12 @@ class Group18Player(BasePokerPlayer):
 
         def getcardx(card):
             suit = card[0]
-            if(suit == 'S'):
-                return 0
-            elif(suit == 'H'):
-                return 1
-            elif(suit=='D'):
-                return 2
-            elif(suit=='C'):
-                return 3
+
+            return Group18Player.suits[suit]
 
         def getcardy(card):
             index = card[1]
-            if(index=='A'):
-                return 12
-            elif(index=='K'):
-                return 11
-            elif(index=='Q'):
-                return 10
-            elif(index=='J'):
-                return 9
-            elif(index=='T'):
-                return 8
-            else:
-                return int(index)-2
+            return Group18Player.ranks[index]
 
         def getstreetgrid(cards):
             grid = np.zeros((4,13))
@@ -91,19 +79,36 @@ class Group18Player(BasePokerPlayer):
             turns = 0
             for action in actions:
                 #max of 12actions per street
-                if ('amount' in action and turns < 6):
-                    image[index,turns] = action['amount'] / eff_stack
+                if 'amount' in action and turns < 6:
+                    image[index, turns] = action['amount'] / eff_stack
                     index += 1
 
-                if(index%2 == 0):
+                if index%2 == 0:
                     index=0
                     turns +=1
 
             return image
 
+        def record_state():
+            # Choose action with highest Q value
+            self.cur_Q_values = self.model.predict(self.sb_features)
+            self.action_sb = np.argmax(self.cur_Q_values)
+
+            if self.has_played:
+                reward_sb = Group18Player.y * np.max(self.cur_Q_values)
+                self.targetQ[0, self.action_sb] = reward_sb
+                self.vvh = self.vvh + 1
+                # new_name = 'my_model_weights'
+                # model.fit(self.old_state,self.targetQ,verbose=0)
+                self.experience_state.append(self.old_state)
+                self.experience_reward.append(self.targetQ)
+                if len(self.experience_state) > Group18Player.max_replay_size:
+                    del self.experience_state[0]
+                    del self.experience_reward[0]
+
         # Maybe don't modularise this, the program takes up more ram when this is modularised
         def pick_action():
-            if np.random.rand(1) < e:
+            if np.random.rand(1) < Group18Player.e:
                 self.action_sb = np.random.randint(0,4)
 
             if self.action_sb == 3 or len(valid_actions) == 2:
@@ -115,7 +120,7 @@ class Group18Player(BasePokerPlayer):
             return action
 
         def save_weights():
-            new_name = datetime.datetime.now().strftime("%d-%m_%H:%M:%S_") + str(self.vvh) + '.h5'
+            new_name = "setup/" + datetime.datetime.now().strftime("%d-%m_%H:%M:%S_") + str(self.vvh) + '.h5'
             self.model.save_weights(new_name)
 
         #####################################################################
@@ -130,42 +135,38 @@ class Group18Player(BasePokerPlayer):
         turn_cards_img = np.zeros((4,13))
         river_cards_img = np.zeros((4,13))
 
+        flop_actions = np.zeros((2,6))
+        turn_actions = np.zeros((2,6))
+        river_actions = np.ones((2,6))
         # self.my_uuid =  round_state['seats'][round_state['next_player']]['uuid']
         # self.my_cards =  hole_card
         # self.community_card = round_state['community_card']
 
-        y = 0.9
-        e = 0.1
         starting_stack = 10000
-        max_replay_size = 40
 
         if self.has_played:
             self.old_state = self.sb_features
-            self.targetQ = self.allQ_sb
-            self.oldAction = self.action_sb
+            self.targetQ = self.cur_Q_values
+            # self.old_action = self.action_sb
 
         sb_position = 1
 
-        flop_actions = np.zeros((2,6))
-        turn_actions = np.zeros((2,6))
-        river_actions = np.ones((2,6))
-
-        preflop_actions = converttoimagemeth(starting_stack,round_state,'preflop')
+        preflop_actions = converttoimagemeth(starting_stack, round_state, 'preflop')
 
         if round_state['street'] == 'flop':
             flop = round_state['community_card']
             flop_cards_img = getstreetgrid(flop)
-            flop_actions = converttoimagemeth(starting_stack,round_state,'flop')
+            flop_actions = converttoimagemeth(starting_stack, round_state, 'flop')
 
         if round_state['street'] == 'turn':
             turn = round_state['community_card'][3]
             turn_cards_img = getstreetgrid([turn])
-            turn_actions = converttoimagemeth(starting_stack,round_state,'turn')
+            turn_actions = converttoimagemeth(starting_stack, round_state, 'turn')
 
         if round_state['street'] == 'river':
             river = round_state['community_card'][4]
             river_cards_img = getstreetgrid([river])
-            river_actions = converttoimagemeth(starting_stack,round_state,'river')
+            river_actions = converttoimagemeth(starting_stack, round_state, 'river')
 
         # Form action features
         actions_feature = np.stack([preflop_actions,flop_actions,turn_actions,river_actions],axis=2).reshape((1,2,6,4))
@@ -183,27 +184,12 @@ class Group18Player(BasePokerPlayer):
         # ENDBLOCK
         #############################################################
 
-        #run model to choose action
-        self.allQ_sb = self.model.predict(self.sb_features)
-        self.action_sb = np.argmax(self.allQ_sb)
-        reward_sb = 0
-
-        if self.has_played:
-            reward_sb += y * np.max(self.allQ_sb)
-            self.targetQ[0, self.action_sb] = reward_sb
-            self.vvh = self.vvh + 1
-            # new_name = 'my_model_weights'
-            # model.fit(self.old_state,self.targetQ,verbose=0)
-            self.experience_state.append(self.old_state)
-            self.experience_reward.append(self.targetQ)
-            if(len(self.experience_state) > max_replay_size):
-                del self.experience_state[0]
-                del self.experience_reward[0]
+        record_state()
 
         self.has_played = True
 
         for ve in range(len(self.experience_state)):
-            self.model.fit(self.experience_state[ve],self.experience_reward[ve],verbose = 0)
+            self.model.fit(self.experience_state[ve], self.experience_reward[ve], verbose=0)
 
         return pick_action()
 
