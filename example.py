694a244f45e559e87a974d93bacce7dbb2b40ad8
jasmoon <fourdusk4@gmail.com>
2019-04-12 08:05:59 +0800

    Moved players into a package except for 'raise_player'

--- example.py
+++ example.py
@@ -1,9 +1,9 @@
 from pypokerengine.api.game import setup_config, start_poker
 from randomplayer import RandomPlayer
 from raise_player import Group18Player
-from honest_player import HonestPlayer
-from reinforced_player import RLPlayer
-from self_player import SelfPlayer
+# from honest_player import HonestPlayer
+# from reinforced_player import RLPlayer
+# from self_player import SelfPlayer
 
 config = setup_config(max_round=1000, initial_stack=10000, small_blind_amount=10)
 
