5c7d29d91e114ce60221f629c297428692f29257
jasmoon <fourdusk4@gmail.com>
2019-04-13 11:59:43 +0800

    test travis

--- raise_player.py
+++ raise_player.py
@@ -235,8 +235,6 @@ class Group18Player(BasePokerPlayer):
         reward = get_real_reward()
 
         print("TARGET Q: {}\t SB_FEATURES: {}".format(self.target_Q, self.sb_features))
-        self.target_Q = self.model.predict(self.sb_features)
-        print("HIHIHIHI{}".format(self.action_sb))
         self.target_Q[0, self.action_sb] = int(reward)
         self.prev_action_state.append(self.sb_features)
         self.prev_reward_state.append(self.target_Q)
